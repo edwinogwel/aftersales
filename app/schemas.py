9176d24ddb5a26d1f51ef60330e6c7b7d22ccf12
edwinogwel <edogwel@gmail.com>
2025-07-28 22:16:22 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -4,15 +4,7 @@ from typing import Optional, Literal
 from enum import Enum
 
 
-# Service Jobs
-class JobStatus(str, Enum):
-    on_hold = "On Hold"
-    pending = "Pending"
-    assigned = "Assigned"
-    in_progress = "In Progress"
-    completed = "Completed"
-
-
+# Service Job
 class JobUpdateRequest(BaseModel):
     job_status: Optional[Literal["On Hold", "Pending",
                                  "Assigned", "In Progress", "Completed"]] = None
@@ -27,6 +19,14 @@ class JobUpdateRequest(BaseModel):
         return self
 
 
+class JobStatus(str, Enum):
+    on_hold = "On Hold"
+    pending = "Pending"
+    assigned = "Assigned"
+    in_progress = "In Progress"
+    completed = "Completed"
+
+
 class Priority(str, Enum):
     low = "Low"
     medium = "Medium"
@@ -38,15 +38,13 @@ class ServiceJob(BaseModel):
     customer: str
     job_status: JobStatus
     priority: Priority
-    # created: automated
     est_completion: date
 
 
-# Service Requests
-class ServiceStatus(str, Enum):
-    pending = "Pending"
-    scheduled = "Scheduled"
-    completed = "Completed"
+# Service Request
+class ServiceUpdateRequest(BaseModel):
+    service_status: Optional[Literal["New", "Waiting for Customer",
+                                     "Waiting for Parts", "In Progress", "Resolved"]] = None
 
 
 class ServiceType(str, Enum):
@@ -55,14 +53,24 @@ class ServiceType(str, Enum):
     inspection = "Inspection"
 
 
+class ServiceStatus(str, Enum):
+    new = "New"
+    pending_customer = "Waiting for Customer"
+    pending_parts = "Waiting for Parts"
+    scheduled = "In Progress"
+    resolved = "Resolved"
+
+
 class ServiceRequests(BaseModel):
     customer: str
     bike_id: str
     service_type: ServiceType
     status: ServiceStatus
-    submitted: date
+    priority: Priority
+    last_updated: date
 
 
+# Job Card
 class JobCard(BaseModel):
     customer: str
     contact: int
